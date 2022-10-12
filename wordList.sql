9c542135fbeeea504f9aba694244b587ff6068a6
Calissa Fairburn <calissa@praxitar.com>
2022-10-12 14:05:30 +0100

    created new table for allowed user words

--- wordList.sql
+++ wordList.sql
@@ -2,14 +2,10 @@ CREATE DATABASE IF NOT EXISTS wordList;
 
 USE wordList;
 
-
 CREATE TABLE words (
     id INT PRIMARY KEY AUTO_INCREMENT,
     word VARCHAR(5) NOT NULL
 );
-
-SHOW TABLES;
-DESCRIBE words;
 
 INSERT INTO words (word)
 VALUES 
@@ -22,5 +18,29 @@ VALUES
 SELECT *
 FROM words;
 
+CREATE TABLE allowed_words (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    word VARCHAR(5) NOT NULL
+);
+
+SHOW TABLES;
+
+ALTER TABLE allowed_words 
+ADD CONSTRAINT UQ_word UNIQUE(word);
+
+ALTER TABLE words 
+ADD CONSTRAINT UQ_word UNIQUE(word);
+
+DESCRIBE allowed_words;
+DESCRIBE words;
+
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
+SET GLOBAL local_infile = 'ON';
+
+LOAD DATA LOCAL INFILE 'C:/Users/cfairburn/Documents/wordle-answers-alphabetical.txt' INTO TABLE allowed_words(word);
+
 SELECT COUNT(id) FROM words;
 
+SELECT word FROM words WHERE id = 3;
+
+SELECT id FROM allowed_words WHERE word = 'aback';
